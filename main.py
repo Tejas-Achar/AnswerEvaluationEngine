5bb7ac782681fde7ea742fbc7362e1b8ae9423aa
Tejas B Achar <30596600+Tejas-Achar@users.noreply.github.com>
2021-10-20 16:05:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,9 @@ def GenerateWordCloudMain():
         Image_Url = "https://drive.google.com/uc?export=view&id=" + str(Image_ID)
         print(Image_Url)
         StudentScore = calculate_score(questionKeywords, modelAnswerKeywords, studentkeywords, Max_Score)
-        return {"url":Image_Url,"score":StudentScore}
+        res = {"url":Image_Url,"score":StudentScore}
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
     def merge_images_top_bottom(file1, file2, cloud_type):
         image1 = Image.open(file1)
         image2 = Image.open(file2)
